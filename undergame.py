1850a77a89ff14c619eddd7cff48e83c2f768dcc
Nunez <Ja.A.Nunez@baypath.net>
2024-11-07 14:12:28 -0500

    collision

--- undergame.py
+++ undergame.py
@@ -3,25 +3,37 @@ import pygame
 
 # pygame setup
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+screen = pygame.display.set_mode((1000, 800))
 clock = pygame.time.Clock()
 running = True
 
 PURPLE=pygame.Color(150,30,245)
 BLACK= pygame.Color(0,0,0)
+GREEN= pygame.Color(0,255,0)
+YELLOW= pygame.Color(255,255,0)
 
+
+
+allThings = []
 
 class Player:
     def __init__(self, rectangle, image, speed):
+        allThings.append(rectangle)
         self.rectangle=rectangle
         self.image=pygame.image.load(image)
         self.image=pygame.transform.scale(self.image, (rectangle.width, rectangle.height))
-        self.speed = 5
+        self.speed = speed
+        self.color=PURPLE
+        self.colliding=False
 
-    
+    def collidingWith(self, rect2):
+        rect=self.rectangle
+        if rect.x<=(rect2.x+rect2.width) and (rect.x+rect.width)>=rect2.x and rect.y<=(rect2.y+rect2.height) and (rect.y+rect.height)>=rect2.y:
+            return True
+            
 
     def move_left(self):
-       self.rectangle.x-=self.speed
+        self.rectangle.x-=self.speed
     def move_right(self):
         self.rectangle.x+=self.speed
     def move_up(self):
@@ -40,7 +52,47 @@ class Player:
             self.move_up()
         if keys_down[pygame.K_DOWN]:
             self.move_down()
+            
+        self.colliding=False
+        for r in allThings:
+            if r!=self.rectangle and self.collidingWith(r):
+                self.colliding=True
 
+    def move2(self):
+        keys_down=pygame.key.get_pressed()
+
+        if keys_down[pygame.K_a]:
+            self.move_left()
+        if keys_down[pygame.K_d]:
+            self.move_right()
+        if keys_down[pygame.K_w]:
+            self.move_up()
+        if keys_down[pygame.K_s]:
+            self.move_down()
+
+        self.colliding=False
+        for r in allThings:
+            if r!=self.rectangle and self.collidingWith(r):
+                self.colliding=True
+
+    def draw(self, screen):
+        rect=self.rectangle
+        if self.colliding:
+            color=YELLOW
+        else:
+            color=PURPLE
+        pygame.draw.rect(screen, color, rect)
+        screen.blit(self.image, (rect.x, rect.y))
+
+class Enemy:
+    def __init__(self,x,y,size):
+        self.x=x
+        self.y=y
+        self.size=size
+
+player1= Player(pygame.Rect(100,100,25,25), "assets/blueSOUL.png", 4)
+player2= Player(pygame.Rect(400,100,25,25), "assets/redSOUL.png", 4)
+allThings.append(pygame.Rect(200,400,25,25))
 
 while running:
     # poll for events
@@ -50,9 +102,23 @@ while running:
             running = False
 
     # UPDATE
+    player1.move2()
+    player2.move()
+    allThings[2].y+=10
+    if allThings[2].y>800:
+        allThings[2].y=-25
+
+    
+            
 
     # DRAW
-    screen.fill(PURPLE)
+    screen.fill(BLACK)
+
+    pygame.draw.rect(screen, GREEN, allThings[2])
+    player1.draw(screen)
+    player2.draw(screen)
+    
+    
 
     # flip() the display to put your work on screen
     pygame.display.flip()
